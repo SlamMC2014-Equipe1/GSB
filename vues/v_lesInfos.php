bfbd8482938e8461d54ff7248ce78b3f0716076e
Christophe Torre <Spasfonx@christophe.torre@hotmail.fr>
2014-05-22 10:31:59 +0200

    Refonte design partie praticiens

--- vues/v_lesInfos.php
+++ vues/v_lesInfos.php
@@ -1,14 +1,12 @@
-<!-- firas le 06/11/2013 -->
-
-
-<div id="contenu">
-    <h2>Informations complémentaires</h2>
-        <TABLE border="1" align="center"> 
-            
-            
-            
- <?php 
-foreach ($lesDetails as $unDetail){
+<div class="td-div">
+<div class="title-bar title-bar-orange">
+    <h1>Praticiens</h1>
+</div>
+<div id="content">
+    <span class="table-title-cadre">Informations complémentaires</span>
+        <table class="table form-table">
+        <?php 
+        foreach ($lesDetails as $unDetail){
             $num= $unDetail["PRA_NUM"];
             $nomPraticien= $unDetail["PRA_NOM"];
             $prenomPraticien= $unDetail["PRA_PRENOM"];
@@ -21,16 +19,16 @@ foreach ($lesDetails as $unDetail){
             $libelleSpecialitePraticien= $unDetail["SPE_LIBELLE"];
             $diplomePraticien= $unDetail["POS_DIPLOME"]; 
             $coefPrescription= $unDetail["POS_COEFPRESCRIPTION"];
-            
-?>
+
+        ?>
             <tr><th>Nom</th><td><?php echo $nomPraticien ?></td>
-            <tr><th>Prénom</th><td><?php echo $prenomPraticien ?></td>
+            <tr class="table-line-grey"><th>Prénom</th><td><?php echo $prenomPraticien ?></td>
             <tr><th>Adresse</th><td><?php echo $adressePraticien ."<br>". $cpPraticien ."<br> ". $villePraticien ?></td>    
-            <tr><th>Coefficient de notoriété</th><td><?php echo $notorietePraticien?></td>
+            <tr class="table-line-grey"><th>Coefficient de notoriété</th><td><?php echo $notorietePraticien?></td>
             <tr><th>Type</th><td><?php echo $typePraticien ?></td>
-            <tr><th>Lieu</th><td><?php echo $lieuPraticien ?></td>
+            <tr class="table-line-grey"><th>Lieu</th><td><?php echo $lieuPraticien ?></td>
             <tr><th>Spécialité</th><td><?php echo $libelleSpecialitePraticien ?></td>
-            <tr><th>Diplôme</th><td><?php echo $diplomePraticien ?></td>  
+            <tr class="table-line-grey"><th>Diplôme</th><td><?php echo $diplomePraticien ?></td>  
             <tr><th>Prescription</th><td><?php echo $coefPrescription ?></td>
                 
 <?php
@@ -39,5 +37,6 @@ foreach ($lesDetails as $unDetail){
      
         </table>
     <br><center><img src="images/fleche.png" width="50" onclick="self.location.href='index.php?uc=praticien&action=liste'"></center>
-</div>    
+</div>
+</div>
  
--- vues/v_lesPraticiens.php
+++ vues/v_lesPraticiens.php
@@ -1,44 +1,57 @@
-<!-- firas le 06/11/2013 -->
-<div id="contenu">
-    <h2>Les praticiens</h2>
-        <table>
-            <tr>
-            <th>Nom</th>
-            <th>Prénom</th>
-            <th>Notoriété</th>
-            <th>Type</th>
-            <th>Lieu </th>
-            <th>Informations</th>
-            </tr>
+<div class="td-div">
+<div class="title-bar title-bar-orange">
+    <h1>Praticiens</h1>
+</div>
+<div id="content">
+    <span class="table-title-cadre">Liste</span>
+    <table class="table table-100">
+        <tr>
+        <th>Nom</th>
+        <th>Prénom</th>
+        <th>Notoriété</th>
+        <th>Type</th>
+        <th>Lieu </th>
+        <th>Informations</th>
+        </tr>
 
 <?php 
+$c = 1;
+$class = '';
 foreach ($lesPraticiens as $unPraticien) {
-            $num= $unPraticien["PRA_NUM"];
-            $nomPraticien= $unPraticien["PRA_NOM"];
-            $prenomPraticien= $unPraticien["PRA_PRENOM"];
-            $adressePraticien= $unPraticien["PRA_ADRESSE"];
-            $cpPraticien= $unPraticien["PRA_CP"];
-            $villePraticien= $unPraticien["PRA_VILLE"];
-            $coefPraticien= $unPraticien["PRA_COEFNOTORIETE"];
-            $libPraticien= $unPraticien["TYP_LIBELLE"];
-            $lieuPraticien= $unPraticien["TYP_LIEU"];
+    $class = $c % 2 == 0 ? 'table-line-grey' : 'table-line';
+    $c++;
+    
+    $num= $unPraticien["PRA_NUM"];
+    $nomPraticien= $unPraticien["PRA_NOM"];
+    $prenomPraticien= $unPraticien["PRA_PRENOM"];
+    $adressePraticien= $unPraticien["PRA_ADRESSE"];
+    $cpPraticien= $unPraticien["PRA_CP"];
+    $villePraticien= $unPraticien["PRA_VILLE"];
+    $coefPraticien= $unPraticien["PRA_COEFNOTORIETE"];
+    $libPraticien= $unPraticien["TYP_LIBELLE"];
+    $lieuPraticien= $unPraticien["TYP_LIEU"];
 ?>
-            <tr> 
-                <td><?php echo $nomPraticien ?></td>
-                <td><?php echo $prenomPraticien ?></td>	
-                <td> 
-                    <span class="stat_notoriete">
-                    <span class="remplissage" style="width : <?php echo (intval($coefPraticien)*180/intval($leMax)).'px'; ?>"><?php echo $coefPraticien ?></span></span>
-                </td>
-                <td><?php echo $libPraticien ?></td>
-                <td><?php echo $lieuPraticien ?></td>
-                <td>
-                    <center><img src="images/information.png" onclic.k="self.location.href='index.php?uc=praticien&num=<?php echo $num ?>&action=detail'"</center>
-                </td>
-            </tr>
+        <tr class="<?php echo $class; ?>"> 
+            <td><?php echo $nomPraticien ?></td>
+            <td><?php echo $prenomPraticien ?></td>	
+            <td> 
+            <acronym title="<?php echo $coefPraticien ?>">
+                <span class="stat_notoriete">
+                    <span class="remplissage" style="width: <?php echo (intval($coefPraticien)*180/intval($leMax)).'px'; ?>"></span>
+                </span>
+            </acronym>
+            </td>
+            <td><?php echo $libPraticien ?></td>
+            <td><?php echo $lieuPraticien ?></td>
+            <td>
+                <a href="index.php?uc=praticien&num=<?php echo $num ?>&action=detail" title="Informations">
+                    <img src="images/information.png" onclic.k="self.location.href='" width="20" height="20"/>
+                </a>
+            </td>
+        </tr>
 <?php 
 }
 ?>
-         </table>
+    </table>
 </div>
-		
+</div>	
